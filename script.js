62f24c8e56198f8c07147485e747a235ca89e473
Bathula Mahendra Kumar <ufindmahendra@gmail.com>
2026-04-11 09:27:22 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,47 @@
+
+// let c =0;
+//  for(var i=2;c<10;i++)
+//  {
+//     let fc=0;
+//        for(var j=1;j<=i;j++)
+//        {
+//         if(i%j==0)
+//         {
+//             fc++;
+//         }
+//        }
+//        if(fc==2)
+//        {
+//         console.log(i);
+//         c++;
+//        }
+//  }
+
+// var a=10;
+// var a=20;
+// console.log(a);
+
+// let c=30;
+// c=40;
+// console.log(c);
+
+// const b=50;
+// console.log(b);
+
+// for(let i=0;i<5;i++)
+// {
+//     for(let j=0;j<5;j++)
+//     {
+//         console.log(i+" ");
+//     }
+//     console.log("\n");
+// }
+
+//Arrays
+let arr=[1,"hii",15.0,true,[1,2,3],'okkk',null];
+console.log(arr);
+for(let i=0;i<arr.length;i++)
+{
+    console.log(typeof arr[i]);
+}
+
