0d9da19266d3476cbd15a12760bb1d9d7d1193b0
Bathula Mahendra Kumar <ufindmahendra@gmail.com>
2026-04-13 09:52:29 +0530

    added script.js

--- script.js
+++ script.js
@@ -60,17 +60,67 @@
 // console.log(obj.loc);
 // obj.loc="hyd";
 // console.log(obj);
-const user=[
-    {
-        id:1, 
-        name:"user1",
-        loc:"hyd"
-    },
-    {
-        id:2,
-        name:"user2",
-        loc:"bag"
-    },
+// const user=[
+//     {
+//         id:1, 
+//         name:"user1",
+//         loc:"hyd"
+//     },
+//     {
+//         id:2,
+//         name:"user2",
+//         loc:"bag"
+//     },
 
-]
-console.log(user[0].name);
+// ]
+// console.log(user[0].name);
+
+// function muFunc()
+// {
+//     console.log("hello");
+//     return -1
+// }
+// console.log(muFunc);
+// console.log(muFunc());
+
+// Function to add two numbers
+// function addNumbers(a, b) {
+//     return a + b;
+// }
+
+// let result = addNumbers(5, 3);
+// console.log("The sum is: " + result);
+
+// const myFun=function(num1,num2) 
+// {
+//     return num1+num2;
+// }   
+// console.log(myFun(10,20));
+
+//   const doMath = (num1, num2, op) => {
+//     switch (op) {
+//         case "+":
+//             console.log(`The addition of ${num1} and ${num2} is ${num1 + num2} `);
+//             break;
+//         case "-":
+//             console.log(`The subtraction of ${num1} and ${num2} is ${num1 - num2} `);
+//             break;
+//         case "*":
+//             console.log(`The multiplication of ${num1} and ${num2} is ${num1 * num2} `);
+//             break;
+//         case "/":
+//             console.log(`The division of ${num1} and ${num2} is ${num1 / num2} `);
+//             break;
+//         case "%":
+//             console.log(`The reminder of ${num1} and ${num2} is ${num1 % num2} `);
+//             break;
+//         case "**":
+//             console.log(`The power of ${num1} and ${num2} is ${num1 ** num2} `);
+//             break;
+//         default:
+//             console.log("Please pass a Valid Input");
+//             break;
+//     }
+// }
+
+// doMath(2, 3, "**")
